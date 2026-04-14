dac07ec03bef31c1bc7ebbb968746c84dd7dcda8
shaank1223 <shaank9511@gmail.com>
2026-04-14 19:56:35 +0530

    fix mongodb connection for vercel

--- database/mongoose.ts
+++ database/mongoose.ts
@@ -1,37 +1,50 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
+if (!MONGODB_URI) {
+  throw new Error("MONGODB_URI must be set within .env");
+}
+
 declare global {
-    var mongooseCache: {
+  // eslint-disable-next-line no-var
+  var mongooseCache:
+    | {
         conn: typeof mongoose | null;
         promise: Promise<typeof mongoose> | null;
-    }
+      }
+    | undefined;
 }
 
 let cached = global.mongooseCache;
 
-if(!cached) {
-    cached = global.mongooseCache = { conn: null, promise: null };
+if (!cached) {
+  cached = global.mongooseCache = {
+    conn: null,
+    promise: null,
+  };
 }
 
 export const connectToDatabase = async () => {
-    if(!MONGODB_URI) throw new Error('MONGODB_URI must be set within .env');
+  // Prevent running on client side
+  if (typeof window !== "undefined") {
+    throw new Error("MongoDB should only run on server side");
+  }
 
-    if(cached.conn) return cached.conn;
+  if (cached?.conn) return cached.conn;
 
-    if(!cached.promise) {
-        cached.promise = mongoose.connect(MONGODB_URI, { bufferCommands: false });
-    }
+  if (!cached?.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      bufferCommands: false,
+    });
+  }
 
-    try {
-        cached.conn = await cached.promise;
-    } catch (err) {
-        cached.promise = null;
-        throw err;
-    }
+  try {
+    cached.conn = await cached.promise;
+  } catch (error) {
+    cached.promise = null;
+    throw error;
+  }
 
-    console.log(`Connected to database ${process.env.NODE_ENV} - ${MONGODB_URI}`);
-
-    return cached.conn;
-}
+  return cached.conn;
+};
