3a0891b9e8a4b9afde07669c332cc231c0b84606
shaank1223 <shaank9511@gmail.com>
2026-04-14 20:49:01 +0530

    fix mongodb connection

--- database/mongoose.ts
+++ database/mongoose.ts
@@ -2,49 +2,23 @@ import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!MONGODB_URI) {
-  throw new Error("MONGODB_URI must be set within .env");
-}
-
-declare global {
-  // eslint-disable-next-line no-var
-  var mongooseCache:
-    | {
-        conn: typeof mongoose | null;
-        promise: Promise<typeof mongoose> | null;
-      }
-    | undefined;
-}
-
-let cached = global.mongooseCache;
+let cached = (global as any).mongoose;
 
 if (!cached) {
-  cached = global.mongooseCache = {
-    conn: null,
-    promise: null,
-  };
+  cached = (global as any).mongoose = { conn: null, promise: null };
 }
 
 export const connectToDatabase = async () => {
-  // Prevent running on client side
-  if (typeof window !== "undefined") {
-    throw new Error("MongoDB should only run on server side");
+  if (!MONGODB_URI) {
+    throw new Error("MONGODB_URI is missing in .env");
   }
 
-  if (cached?.conn) return cached.conn;
+  if (cached.conn) return cached.conn;
 
-  if (!cached?.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, {
-      bufferCommands: false,
-    });
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI);
   }
 
-  try {
-    cached.conn = await cached.promise;
-  } catch (error) {
-    cached.promise = null;
-    throw error;
-  }
-
+  cached.conn = await cached.promise;
   return cached.conn;
 };
